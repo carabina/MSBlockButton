46a33a194cb88771aeae7aea61d6279ca16b4206
jiang.zijia <jiangzijia>
2018-03-08 01:09:26 +0800

    init.

---
+++ MSBlockButtonDemo/SwiftViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SwiftViewController.swift
+//
+//
+//  Created by MOMO on 2017/4/24.
+//
+//
+
+import UIKit
+
+class SwiftViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "touchesBegan to pop"
+        view.backgroundColor = UIColor.white
+        view.addSubview(self.button)
+        
+        self.button.handleTouchUpInsideEvent { (_) in
+            print("\("SwiftViewController") \(#function)")
+
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    lazy var button:UIButton = {
+        let btn = UIButton(frame: CGRect(origin: CGPoint.zero, size: CGSize(width: 100, height: 100)))
+        btn.backgroundColor = UIColor.purple
+        btn.setTitle("SwiftButton", for: UIControlState.normal)
+        btn.center = self.view.center
+        self.view.addSubview(btn)
+        return btn;
+    }()
+    
+}
---
+++ MSBlockButtonDemo/ViewController.m
@@ -0,0 +1,58 @@
+//
+//  ViewController.m
+//  MSBlockButtonDemo
+//
+//  Created by MOMO on 2017/4/23.
+//  Copyright © 2017年 MOMO. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "MSBlockButton.h"
+#import "MSBlockButtonDemo-Swift.h"
+
+#define MDScreenWidth     CGRectGetWidth([[UIScreen mainScreen] bounds])
+#define MDScreenHeight    CGRectGetHeight([[[UIApplication sharedApplication].delegate window] bounds])
+@interface ViewController ()
+@property(nonatomic,strong)UIButton* button;
+@end
+
+@implementation ViewController
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.title = @"touchesBegan to SwiftVC";
+    [self.view addSubview:self.button];
+    
+    //default UIControlEventTouchUpInside
+    [self.button handleTouchUpInsideEventWithBlock:^(UIButton *sender) {
+        NSLog(@"ViewController %s",__func__);
+        
+    }];
+    
+    
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    [self.navigationController pushViewController:[SwiftViewController new] animated:YES];
+}
+
+
+- (UIButton *)button{
+    if(!_button){
+        
+        UIButton* button = [UIButton buttonWithType:UIButtonTypeCustom];
+        button.frame = CGRectMake((MDScreenWidth-100)*0.5, (MDScreenHeight-100)*0.5, 100, 100);
+        [self.view addSubview:button];
+        button.titleLabel.numberOfLines = 0;
+        button.titleLabel.textAlignment = NSTextAlignmentCenter;
+        button.backgroundColor = [[UIColor orangeColor] colorWithAlphaComponent:0.8];
+        [button setTitle:@"Objective-C Button" forState:UIControlStateNormal];
+        
+        _button = button;
+    }
+    return _button;
+}
+
+@end
+
